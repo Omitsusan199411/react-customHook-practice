e1b47538858f53ddadc86b61e64a31c9c0a6dfee
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-06-05 15:17:23 +0900

    UserCardコンポーネントを作成 カスタムフックの準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import React, { useState } from "react";
 import "./styles.css";
+import { VFC } from "react";
+import { UserCard } from "./components/UserCard";
+import { UserProfile } from "./types/userProfile";
+import { User } from "./types/api/user";
+import axios from "axios";
 
-export const App = () => {
+export const App: VFC = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.city}`
+        }));
+        setUserProfiles(data);
+      });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 };
